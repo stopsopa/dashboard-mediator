68db480bd91b486a1da6f180eb91c1fadb283325
Simon <szymon.dzialowski@gmail.com>
2019-01-05 23:38:53 +0000

    update

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+u: # push to github & release in heroku
+	/bin/bash update.sh
+t:
+	/bin/bash test.sh
--- controllers/test.js
+++ controllers/test.js
@@ -1,5 +1,4 @@
 
-    
 // https://nodejs.org/api/http.html#http_class_http_serverresponse
     
 const controller = (req, res, query = {}) => {
@@ -7,7 +6,7 @@ const controller = (req, res, query = {}) => {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 
-    res.statusCode = 404;
+    //res.statusCode = 404;
 
     res.end(JSON.stringify({
         page: {
