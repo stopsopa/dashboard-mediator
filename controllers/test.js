efc052fc0b23cce246d25446b0837c5a6539c6c2
Simon <szymon.dzialowski@gmail.com>
2019-01-05 22:35:29 +0000

    server.js

---
+++ controllers/test.js
@@ -0,0 +1,22 @@
+
+    
+// https://nodejs.org/api/http.html#http_class_http_serverresponse
+    
+const controller = (req, res, query = {}) => {
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    res.statusCode = 404;
+
+    res.end(JSON.stringify({
+        page: {
+            query,
+        },
+        v: process.version
+    }));
+}
+
+controller.url = '/test';
+
+module.exports = controller;
